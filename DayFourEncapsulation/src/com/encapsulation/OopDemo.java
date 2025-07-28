86a405d38908a7e579f984afc6e923d0e96ec5e4
Vaishnavi-Shinde4 <vaishashinde04@gmail.com>
2025-07-28 22:28:20 +0530

    Add files via upload

---
+++ DayFourEncapsulation/src/com/encapsulation/OopDemo.java
@@ -0,0 +1,48 @@
+package com.encapsulation;
+
+public class OopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private int serialNum;
+	private String name;
+	private int Age;
+	
+	public void setSerialNum(int serialNum)
+	{
+		this.serialNum = serialNum;
+	}
+	
+	public int getSerialNum()
+	{
+		return serialNum;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setAge(int Age)
+	{
+		this.Age = Age;
+	}
+	
+	public int getAge()
+	{
+		return Age;
+	}
+	
+	//public String toString
+	{
+		//return "OopDemo [serialNum="+serialNum+"],[name="+name+"],[Age="+Age+"]";
+	}
+}
